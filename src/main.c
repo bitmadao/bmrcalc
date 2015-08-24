fba980ad64e1ba362b0e93f7c3663a00e9fa2873
Dag Soervoll <dag@pepmagic.no>
2015-08-24 22:44:53 +0200

    add more comments to main.c

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int main(void) {
 	/* fetching data */
 	printf("bmrcalc can calculate your BasalMetabolicRate\nLet's start off with you telling us your gender, m(ale) or f(emale), and age (years):") ;
 	
-	FINDGENDER: /* in case of unrecognized gender */
+	FINDGENDER: /* in case of unrecognized gender, user must goto this marker */
 	
 	scanf("%c%d", &gender, &age) ;
 	
@@ -26,13 +26,16 @@ int main(void) {
 	
 	scanf("%lf%lf", &weight, &height) ;
 	
+	/* crunching numbers */
 	if(gender == 'f') {
-		bmrInKj = ((42 * weight) + (26.3 * height) - (20,7 * age) - 676.2) ;
+		bmrInKj = ((42 * weight) + (26.3 * height) - (20,7 * age) - 676.2) ; /* Female calculation */
 	} else {
-		bmrInKj = ((42 * weight) + (26.3 * height) - (20,7 * age) + 697.2 - 676.2) ;
+		bmrInKj = ((42 * weight) + (26.3 * height) - (20,7 * age) + 697.2 - 676.2) ; /* Male calculation */
 	}
 	
 	bmrInKcal = bmrInKj / 4.2 ;
+	
+	/* presenting data */
 	
 	printf("The numbers have been crunched!\nYour BMR is %lfkcal (%lfkj).\nGood health to you!\n", bmrInKcal, bmrInKj) ;
 	return 0 ;
